cf734e5ca9e5e288b9acebef7ddba586dd4de8d6
Sachin aralapura <89058705+sachinaralapura@users.noreply.github.com>
2023-01-11 23:53:49 +0530

    added more logic and password hashing
    >password are hashed before inserting into login table
    >added logic to check login credentials  with existing login information in database and show error if not matching
    >added logic to insert login credentials when new user register (inserting email name and hashed password)
    >added SESSION superglobals

--- logreg/testlogreg.php
+++ logreg/testlogreg.php
@@ -1,5 +1,5 @@
 <?php
-
+    session_start();
 
 
     if(isset($_POST['log-submit'])){
@@ -15,84 +15,64 @@
             exit();
         }
         
-        echo $log_email;
-        echo $log_password;
-        $sql = "SELECT * FROM login WHERE email = '$log_email' AND password='$log_password' ";
+    
+        $sql = "SELECT * FROM login WHERE email = '$log_email'";
         $result = mysqli_query($conn,$sql) or die("query failed");
         $count=mysqli_num_rows($result);
-        print_r($result);        
-        print_r($count);
-        if($count>0){
-            echo "login successful";
-        }
-        else{
-             echo "login unsuccessful";
-        }
+        $row = mysqli_fetch_assoc($result);
+          if($count==1){
+            if(password_verify($log_password,$row['password'])){
+                $message = "login successful";
+                //$_SESSION['loginsucess']=$message;
+                echo $message;
+            }
+            else{
+                $unsucess = "login unsuccessful";
+               $_SESSION['loginunsucess']=$unsucess;
+               header("location:logreg.php");
+            }
+              
+          }
+          else{
+               $unsucess = "login unsuccessful";
+               $_SESSION['loginunsucess']=$unsucess;
+               header("location:logreg.php");
+          }
         
     }
            
            
     if(isset($_POST['sign-submit'])){
         require '../dbinc.php';
-        echo "success";
-    }
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-            // $sql = "SELECT * FROM login WHERE email = ?";
-            // //create a prepared statement
-            // $stmt  = mysqli_stmt_init($conn);
-            // //prepare the prepared statement
-            // if(!mysqli_stmt_prepare($stmt,$sql)){
-            //     echo "sql statement failed";
-            // }else{
-            //     echo "sucess";
-            //     //bind parameters to the placeholders
-            //     mysqli_stmt_bind_param($stmt,"s",$log_email);
-            //     //Run parameters inside database
-            //     mysqli_stmt_execute($stmt);
-            //     $row = mysqli_stmt_get_result($stmt);
-            // }
-            // $row = mysqli_fetch_assoc($row);
-            
-            // echo is_array($row);
+        $user_name =$_POST['sign-name'];
+        $user_email=$_POST['sign-email'];
+        $user_pass=$_POST['sign-pass'];
+        $user_cpas=$_POST['sign-cpass'];
 
+        $sql ="SELECT * FROM login WHERE email = '$user_email'";
+        $result=mysqli_query($conn,$sql);
+        $row = mysqli_num_rows($result);
+        if($row>0){
+            $message = "Email already registered";
+            $_SESSION['emailexists']= $message;
+            header("location:logreg.php");
+        }else{
+           if($user_pass==$user_cpas){
+                $hash = password_hash($user_pass,PASSWORD_DEFAULT);
+                $insert = "INSERT INTO login (email,name,password) VALUES ( '$user_email','$user_name','$hash')";
+                $result = mysqli_query($conn,$insert);
+                if($result){
+                    echo "user creation sucessful";
+                }
+           }else{
+            $mismatch = "Password not matching";
+            $_SESSION['mismatch']=$mismatch;
+            header("location:logreg.php");
+           }
+
+        }
         
-            //print_r($row['password']);
-            // if($row["email"]=="sachin.aralapura@gmail.com"){
-            //     echo "<h1> admim </h1> ";
-            // }
+    }
             
-       
-        
-           
     
 ?>  
